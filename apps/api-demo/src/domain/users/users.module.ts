64228ee846bb38b662607eebf2792a05c3ddf568
josetrejo <josers77.1@gmail.com>
2024-07-26 20:38:11 -0600

    added mongodb

--- apps/api-demo/src/domain/users/users.module.ts
+++ apps/api-demo/src/domain/users/users.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './service/users.service';
 import { UsersController } from './controller/users.controller';
 import { Repository } from 'typeorm';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, Repository],  
 })
